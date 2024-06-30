f56bd68f47399416a4a88beda413a1ade209d913
sc2213404 <2213404@mail.nankai.edu.cn>
2024-06-30 18:16:44 +0800

    Add files via upload

---
+++ final_version/html/map.js
@@ -0,0 +1,9 @@
+onButtonClick() 
+{
+    const key = 'CQSBZ-AFJ3V-6LFPB-5YCAM-C7QH6-KLFL2'; // 使用在腾讯位置服务申请的key
+    const referer = '规途'; // 调用插件的app的名称
+    const location = JSON.stringify({
+      latitude: 39.89631551,
+      longitude: 116.323459711
+    }
+}
